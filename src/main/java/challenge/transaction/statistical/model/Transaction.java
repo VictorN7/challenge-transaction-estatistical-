905866e575bc95d9a5b0996354aec0354e2e7ad4
VictorN7 <victorhnogueira@hotmail.com>
2026-01-04 02:32:05 -0300

    Criar entidades de dominio e definir pacotes

---
+++ src/main/java/challenge/transaction/statistical/model/Transaction.java
@@ -0,0 +1,28 @@
+package challenge.transaction.statistical.model;
+
+import java.time.OffsetDateTime;
+import jakarta.validation.constraints.NotBlank;
+
+public class Transaction {
+	
+	@NotBlank
+	private Double value; 
+	@NotBlank
+	private OffsetDateTime dateTime;
+	
+	public Transaction() {	
+	}
+	
+	public Transaction(Double value, OffsetDateTime dateTime) {
+		this.value = value;
+		this.dateTime = dateTime;
+	}
+
+	public Double getValue() {
+		return value;
+	}
+
+	public OffsetDateTime getDateTime() {
+		return dateTime;
+	}
+}
