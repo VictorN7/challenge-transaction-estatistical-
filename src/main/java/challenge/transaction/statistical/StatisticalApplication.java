dbbd3a91dc59ec632e02bf0bcba69c1ee09818ac
VictorN7 <victorhnogueira@hotmail.com>
2026-01-04 02:14:03 -0300

    initial commit

---
+++ src/main/java/challenge/transaction/statistical/StatisticalApplication.java
@@ -0,0 +1,13 @@
+package challenge.transaction.statistical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatisticalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatisticalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/challenge/transaction/statistical/StatisticalApplicationTests.java
@@ -0,0 +1,13 @@
+package challenge.transaction.statistical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatisticalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
